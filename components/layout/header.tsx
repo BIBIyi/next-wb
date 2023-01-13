9efe10bdb3c372fdb196effe2ee63e5de1526bc6
Emi Zhu <385115337@qq.com>
2023-01-13 21:13:42 +1100

    start header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import Head from "next/head";
+import Link from "next/link";
 
 export default function header() {
-  return <div>header</div>;
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <div>header</div>
+    </>
+  );
 }
